e8b44aebba9582ac7a908291db8f2fd19f80b2f9
OLI ULLAH <oli.ullah@datasoft-bd.com>
2025-02-11 17:19:13 +0600

    Initial Commit

---
+++ app/src/main/java/dewan/software/dewan_expense/MainActivity.kt
@@ -0,0 +1,47 @@
+package dewan.software.dewan_expense
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dewan.software.dewan_expense.ui.theme.DEWAN_EXPENSETheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DEWAN_EXPENSETheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DEWAN_EXPENSETheme {
+        Greeting("Android")
+    }
+}
